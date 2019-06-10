2558b4c660c031734dd2890ec46a55013023c60c
Magical <renxiaoya168@163.com>
2019-06-11 00:15:53 +0800

    新建页面title输入框，未完成

--- lib/page/main_add_page.dart
+++ lib/page/main_add_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:goanddo/navigator/side-drawer.dart';
 
@@ -8,13 +7,37 @@ class MainAddPage extends StatefulWidget {
 }
 
 class _MainAddPageState extends State<MainAddPage> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('新建'),
-      ),
-      body: Center(child: Text('新建'),),
-    );
+        appBar: AppBar(
+          title: Text('新建'),
+        ),
+        body: Form(
+            key: _formKey,
+            child: Container(
+              decoration: BoxDecoration(),
+              padding: EdgeInsets.fromLTRB(10, 20, 10, 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10))),
+//                      icon: Icon(Icons.title),
+                      hintText: '未命名的项'
+                    ),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return '请输入标题';
+                      }
+                    },
+                  ),
+                ],
+              ),
+            )));
   }
 }
